46a0a35217976125af9bb197ea9fe3273dcff6bd
Ankiwoong <ankiwoong@gmail.com>
2020-06-13 17:44:43 +0900

    Multiple Views

--- try_django/views.py
+++ try_django/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 # Model View Template (MVT)
 def home_page(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def about_page(request):
+    return HttpResponse("<h1>About Us</h1>")
+
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Us</h1>")
