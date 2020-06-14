d7d6cd2741aef3e3d99e27106794fb818cefcb2f
Ankiwoong <ankiwoong@gmail.com>
2020-06-15 07:49:57 +0900

    Dynamic URL-based Lookups

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from .models import BlogPost
 # Create your views here.
 
 
-def blog_post_detail_page(request):
-    obj = BlogPost.objects.get(id=1)  # query > database > data > django renders
+def blog_post_detail_page(request, post_id):
+    obj = BlogPost.objects.get(id=post_id)  # query > database > data > django renders
     template_name = "blog_post_detail.html"
     context = {"object": obj}  # {'title': objecct.title}
     return render(request, template_name, context)
--- try_django/urls.py
+++ try_django/urls.py
@@ -26,7 +26,9 @@ from .views import (
 
 urlpatterns = [
     path("", home_page),
-    path("blog/", blog_post_detail_page),
+    # path("blog/", blog_post_detail_page),
+    path("blog/<int:post_id>/", blog_post_detail_page),
+    # re_path(r"^blog/(?P<post_id>\w+)/$", blog_post_detail_page),
     path("page", about_page),
     path("pages", about_page),
     re_path(r"^pages?/$", about_page),
