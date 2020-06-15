c713da5118d51339c3b6df237ac6493b8c6e6877
Ankiwoong <ankiwoong@gmail.com>
2020-06-16 07:06:40 +0900

    QuerySet Lookups

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,19 @@ from .models import BlogPost
 
 # Create your views here.
 
+# GET > 1 object            : 1개 오브젝트 수용
+# filter > [] object        : 리스트 형태 오브젝트 수용
+
 
 def blog_post_detail_page(request, slug):
-    # obj = BlogPost.objects.get(slug=slug)
+    queryset = BlogPost.objects.filter(slug=slug)
+    if queryset.count() >= 1:
+        # raise Http404
+        obj = queryset.first()
+
+    # obj = queryset.first()
     # print(post_id.__class__)
-    obj = get_object_or_404(BlogPost, slug=slug)
+    # obj = get_object_or_404(BlogPost, slug=slug)
     template_name = "blog_post_detail.html"
     context = {"object": obj}  # {'title': objecct.title}
 
