d51c643bb85cf5da02ab32d1b5842e99172b6467
Ankiwoong <ankiwoong@gmail.com>
2020-06-16 17:56:21 +0900

    Include URLs

--- try_django/settings.py
+++ try_django/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [  # components
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog",
+    "blog",  # pluggable
 ]
 
 MIDDLEWARE = [
