e5865abfdd6669cf930c1cda0d946e864ef29d7f
Ankiwoong <ankiwoong@gmail.com>
2020-06-18 12:13:17 +0900

    Update View with Model Form

--- blog/forms.py
+++ blog/forms.py
@@ -16,11 +16,11 @@ class BlogPostModelForm(forms.ModelForm):
         model = BlogPost
         fields = ["title", "slug", "content"]
 
-    def clean_title(self, *args, **kwargs):
-        title = self.cleaned_data.get("title")
-        qs = BlogPost.objects.filter(title=title)
-        if qs.exists():
-            raise forms.ValidationError(
-                "This title has already been used. Please try again."
-            )
-        return title
+    # def clean_title(self, *args, **kwargs):
+    #     title = self.cleaned_data.get("title")
+    #     qs = BlogPost.objects.filter(title=title)
+    #     if qs.exists():
+    #         raise forms.ValidationError(
+    #             "This title has already been used. Please try again."
+    #         )
+    #     return title
