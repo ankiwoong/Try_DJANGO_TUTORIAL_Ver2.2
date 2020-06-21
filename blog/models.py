44ee72a5c9e8b4234de8dc2b0887928b47b40d51
Ankiwoong <ankiwoong@gmail.com>
2020-06-21 10:29:17 +0900

    Complex Lookups

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.utils import timezone
 
 # Create your models here.
@@ -12,6 +13,18 @@ class BlogPostQuerySet(models.QuerySet):
         now = timezone.now()
         return self.filter(publish_date__lte=now)
 
+    def search(self, query):
+        lookup = (
+            Q(title__icontains=query)
+            | Q(content__icontains=query)
+            | Q(slug__icontains=query)
+            | Q(user__first_name__icontains=query)
+            | Q(user__last_name__icontains=query)
+            | Q(user__username__icontains=query)
+        )
+
+        return self.filter(lookup)
+
 
 class BlogPostManager(models.Manager):
     def get_queryset(self):
@@ -20,11 +33,16 @@ class BlogPostManager(models.Manager):
     def published(self):
         return self.get_queryset().published()
 
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().published().search(query)
+
 
 class BlogPost(models.Model):  # blogpost_set -> queryset
     # id = models.IntegerField() # pk
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
-    image = models.FileField(upload_to="image/", blank=True, null=True)
+    image = models.ImageField(upload_to="image/", blank=True, null=True)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)  # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
